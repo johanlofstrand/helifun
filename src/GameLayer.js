52ae2ba6879bf11827a6a55f97aa243de31b85e7
Johan Löfstrand <johan.lofstrand@gmail.com>
2015-06-18 23:43:11 +0200

    minor fixes

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -9,10 +9,10 @@ import ui.resource.Image as Image;
 
 	exports.populateGameLayer = function (layer, x) {
 
-		var yfactor = 5;
+		var yfactor = 7;
 		var yspread_tree = util.randInt(5,10);
 		var size = util.choice(['S', 'M', 'L', 'XL']);
-		var space_between = util.randInt(200,600);
+		var space_between = util.randInt(300,800);
 		var img = new Image({url: "resources/images/level/mountain_" + size + ".png"});
 		var mountain_height = img.getHeight();
 		var mountain = layer.obtainView(ui.ImageView, {
@@ -26,12 +26,12 @@ import ui.resource.Image as Image;
 
 		Physics.addToView(mountain, {group: "ground"});
 
-		if (util.randInt(1,3) === 1) {
+		if (util.randInt(1,3) === 1 && size!= 'L') {  //L sized mountain not good fit for tree...
 			var tree = layer.obtainView(ui.ImageView, {
 				superview: layer,
 				image: "resources/images/level/jtree1.png",
-				x: x + 550 + util.randInt(40, 150),
-				y: layer.style.height - 40 - mountain_height -  yfactor - yspread_tree,
+				x: x + 550 + util.randInt(80, 150),
+				y: layer.style.height - 40 - mountain_height - yfactor - yspread_tree,
 				autoSize: true	 //image size = view size
 			});
 		}
